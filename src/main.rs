a178c238483e38f0096febdbf5a0b2023e2094e7
Clément Guiton <clement.guiton.dev@gmail.com>
2023-07-19 21:31:02 +0200

    <feat>(Kafka) consumer

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,31 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use tokio::join;
 
 use std::net::SocketAddr;
 
 mod input_transaction;
 mod kafka_producer;
+mod kafka_consumer;
 
 use input_transaction::InputTransaction;
 use kafka_producer::add_input_transaction;
+use kafka_consumer::subscribe_input_transactions;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    
     let app = Router::new().route("/", post(receive_transaction));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let axum_future = axum::Server::bind(&addr)
+        .serve(app.into_make_service());
+
+    let consumer_future = subscribe_input_transactions("localhost:29092");
+    join!(axum_future, consumer_future);
 }
 
 async fn receive_transaction(Json(payload): Json<InputTransaction>) -> impl IntoResponse {
